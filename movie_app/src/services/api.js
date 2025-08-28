19fe33da78564d15da8c478a843192bbfdba1555
Kheervin12 <kevinkimon@gmail.com>
2025-08-28 19:58:28 +0100

    movie app

---
+++ movie_app/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "52febc34260c2f4722eabe225526996b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
